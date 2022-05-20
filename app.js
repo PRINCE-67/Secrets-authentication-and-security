ac840a8a7f304a2b1fb4e3d8d3920207b8ecf6fa
Prince Verma <princeverma694269@gmail.com>
2022-05-20 16:06:26 +0530

    cookies and session added

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session=require("express-session");
+const passport=require("passport");
+const passportLocalMongoose=require("passport-local-mongoose");
 
 const app = express();
 
@@ -15,6 +16,16 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+// creates a session
+app.use(session({
+  secret:"princeisawesomealways.",
+  resave: false,
+  saveUninitialized:false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://localhost:27017/userDB");
 
 const userSchema = new mongoose.Schema({
@@ -22,7 +33,18 @@ const userSchema = new mongoose.Schema({
   password: String
 });
 
+//it does hashing and salting and saving the users into out databases.
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model("User", userSchema);
+
+//creating the strategies for our user
+passport.use(User.createStrategy());
+
+//ceating the fortune cookie and stuffing with user credentials
+passport.serializeUser(User.serializeUser());
+//destroying the cookie
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
   res.render("home.ejs");
@@ -32,42 +54,55 @@ app.get("/login", function(req, res) {
   res.render("login.ejs");
 });
 
-app.post("/login", function(req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-  User.findOne({email: username}, function(err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, function(err, result) {
-          if (result === true) {
-            res.render("secrets.ejs");
-          }
-        });
-      }
-    }
-  });
-});
-
 app.get("/register", function(req, res) {
   res.render("register.ejs");
 });
 
-app.post("/register", function(req, res) {
-  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    });
+app.get("/secrets",function(req,res){
+  if(req.isAuthenticated()){
+    res.render("secrets.ejs");
+  }
+  else{
+    res.redirect("/login");
+  }
+});
 
-    newUser.save(function(err) {
-      if (err)
-        console.log(err);
-      else {
-        res.render("secrets.ejs");
-      }
-    });
+app.get("/logout",function(req,res){
+  req.logout();
+  res.redirect("/");
+});
+
+app.post("/register", function(req, res) {
+  User.register({username:req.body.username},req.body.password,function(err,user){
+    if(err){
+      console.log(err);
+      res.redirect("/register");
+    }
+    else{
+      // create a cookie and save their session as logged-in.
+      // authenticate means to get one's identity verified.
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/secrets");
+      });
+    }
+  });
+
+});
+
+app.post("/login", function(req, res) {
+  const user=new User({
+    username:req.body.username,
+    password:req.body.password
+  });
+
+  req.login(user,function(err){
+    if(err)
+    console.log(err);
+    else{
+      passport.authenticate("local")(req,res,function(){
+        res.redirect("/secrets");
+      });
+    }
   });
 });
 
